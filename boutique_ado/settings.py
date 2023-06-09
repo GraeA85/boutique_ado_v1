d3b2789692e2de023180bd5b57306896f113af61
GraeA85 <graemearthur36@gmail.com>
2023-06-09 14:40:00 +0000

    add forms to checkout

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,9 +22,9 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '8000-graea85-boutiqueadov1-5w67jsv3wtr.ws-eu98.gitpod.io']
+ALLOWED_HOSTS = ['localhost', '8000-graea85-boutiqueadov1-5w67jsv3wtr.ws-eu98.gitpod.io', '8000-graea85-boutiqueadov1-5w67jsv3wtr.ws-eu99.gitpod.io']
 
-CSRF_TRUSTED_ORIGINS = ['https://8000-graea85-boutiqueadov1-5w67jsv3wtr.ws-eu98.gitpod.io']
+CSRF_TRUSTED_ORIGINS = ['https://8000-graea85-boutiqueadov1-5w67jsv3wtr.ws-eu98.gitpod.io', 'https://8000-graea85-boutiqueadov1-5w67jsv3wtr.ws-eu99.gitpod.io']
 
 # Application definition
 
