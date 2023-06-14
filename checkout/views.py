d929e5f62b20021ba0899cf7da5b6c4a0111bf38
GraeA85 <graemearthur36@gmail.com>
2023-06-14 14:25:54 +0000

    add stripe payment system to page

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NIuDLCHimmDlHcqrwQFvPnRuLSEt0UJ9pO23vOWJNvPmJ651HKVJR0gUWbDJDRO3pMddktZF5xP3IlZ2Wd2ZB9v00SmHtFVQl',
+        'client_secret': 'test client secret', 
     }
 
     return render(request, template, context)
